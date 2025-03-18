b7f374497c68a0cce51bf1ab32014e75ecd64fa2
Jack625a <ktfut15@gmail.com>
2025-03-18 16:06:19 -0400

    Sintaxis Python

---
+++ prueba.py
@@ -0,0 +1,33 @@
+print("Codigo en Python")
+#SINTAXIS BASICA DE PYTHON
+#VARIABLES - TIPOS DE DATOS
+#tipos de datos (numericos, cadena caracteres, booleanos)
+numero=15
+numero2= 15.2
+nombre="Kevin"
+nombre2='Juan'
+numero3='5'
+encender=True #False
+#Tipos de Datos Complejos
+#Listas [], Tuplas (), Diccionarios {}
+colores=["rojo","verde","azul"]
+lista=[455,12.5,"abc",True]
+#Acciones para una lista
+print(lista)
+#Insercion de datos
+lista.append("Hola")
+print(lista)
+#Insert
+lista.insert(1,"hola2")
+print(lista)
+lista.insert(1,"hola3")
+print(lista)
+#mostrar el ultimo elemento de una lista
+print(lista[-1])
+#longitud de una lista count
+#len (longitud de elemento)
+print(len(lista))
+
+#TUPLAS
+numeros4=(45,68,636,6,5,3)
+
